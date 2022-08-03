6ec21ba507ad01cf280b06bd16bf2c2d1cfcf8ff
vusalrahimli1 <vusalrahimli999@gmail.com>
2022-08-03 19:07:09 +0400

    First Commit

---
+++ Client/src/main/java/com/mycompany/client/Client.java
@@ -0,0 +1,76 @@
+package com.mycompany.client;
+
+import java.io.BufferedWriter;
+import util.FileUtility;
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+import lombok.SneakyThrows;
+
+public class Client {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("What do you want to send? (1 - Image, 2 - Message): ");
+
+        int choice = scn.nextInt();
+        switch (choice) {
+            case 1:
+                sendImage("localhost", 2001, "C:/Users/VUSAL/Downloads/tcp/Client/2020_01_23 21_57 Office Lens.jpg");
+                break;
+            case 2:
+                sendMessage("localhost", 2001);
+                break;
+            default:
+                System.out.println("Incorrect choice!");
+                break;
+        }
+    }
+
+    @SneakyThrows
+    public static void sendImage(String ip, int port, String imagePath) {
+
+        Socket socket = new Socket(ip, port);
+
+        OutputStream outputStream = socket.getOutputStream();
+
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+        byte[] bytes = FileUtility.readBytes(imagePath);
+
+        dataOutputStream.writeInt(bytes.length);
+        dataOutputStream.write(bytes);
+
+        socket.close();
+
+        System.out.println("Sending...");
+        Thread.sleep(2000);
+        System.out.println("The image was successfully sent to the client.");
+
+    }
+
+    @SneakyThrows
+    public static void sendMessage(String ip, int port) {
+        Scanner scn = new Scanner(System.in);
+        while (true) {
+            System.out.print("Type message: ");
+            String message = scn.nextLine();
+
+            Socket s = new Socket(ip, port);
+
+            OutputStream ous = s.getOutputStream();
+            BufferedWriter b;
+            DataOutputStream dout = new DataOutputStream(ous);
+
+            dout.write(message.getBytes());
+
+            s.close();
+            if (message.equalsIgnoreCase("bye")) {
+                break;
+            }
+        }
+    }
+
+}
---
+++ Server/src/main/java/com/mycompany/server/Server.java
@@ -0,0 +1,95 @@
+package com.mycompany.server;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import lombok.SneakyThrows;
+import util.FileUtility;
+
+public class Server {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("What will you accept? (1 - Image, 2 - Message): ");
+
+        int choice = scn.nextInt();
+        switch (choice) {
+            case 1:
+                receiveImage(2001, "C:/Users/VUSAL/Downloads/tcp/Server/vusal.jpg");
+                break;
+            case 2:
+                receiveMessage(2001);
+                break;
+            default:
+                System.out.println("Incorrect choice!");
+                break;
+        }
+        //"/path/to/store/image.jpg"
+    }
+
+    @SneakyThrows
+    public static void receiveImage(int port, String imagePath) {
+
+        ServerSocket ss = new ServerSocket(port);
+
+        System.out.println("Server is Waiting for request...");
+
+        Socket connection = ss.accept();
+
+        System.out.println("Connecting...");
+        Thread.sleep(2000);
+        System.out.println("Connected with: " + connection.getInetAddress());
+
+        DataInputStream dis = new DataInputStream(connection.getInputStream());
+
+        byte[] arr = readMessage(dis);
+
+        FileUtility.writeBytes(arr, imagePath);
+        System.out.println("\nServer: The image was successfully received from the client");
+
+        //"C:/Users/VUSAL/Downloads/tcp/Client/2020_01_23 21_57 Office Lens.jpg"
+        //"C:/Users/VUSAL/Downloads/tcp/Server/vusal.jpg"
+    }
+
+    @SneakyThrows
+    public static byte[] readMessage(DataInputStream din) {
+        int msgLen = din.readInt();
+
+        byte[] msg = new byte[msgLen];
+
+        din.readFully(msg);
+        return msg;
+    }
+
+    @SneakyThrows
+    public static void receiveMessage(int port) {
+        ServerSocket ss = new ServerSocket(port);
+
+        System.out.println("Server is Waiting for request...\n");
+        while (true) {
+            Socket s = ss.accept();
+
+            InputStream is = s.getInputStream();
+
+            InputStreamReader isr = new InputStreamReader(is);
+
+            BufferedReader br = new BufferedReader(isr);
+
+            String client_message = br.readLine();
+
+            System.out.println("Client: " + client_message);
+
+            if (client_message.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+        }
+    }
+
+}
